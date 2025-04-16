c1e0db0ab9784514dc13dc7741002b22ad0a15d2
shimateruki <k024g0032@g.neec.ac.jp>
2025-04-16 15:47:09 +0900

    logと実装と関数を作った

--- main.cpp
+++ main.cpp
@@ -1,5 +1,51 @@
 ﻿#include <windows.h>
 #include <cstdint>
+#include <filesystem>
+#include <string>
+#include <chrono>
+#include <format>
+
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
+
+
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+
+//void Log(std::ostream& os, const std::string& message)
+//{
+//	os << message << std::endl;
+//	OutputDebugStringA(message.c_str());
+//}
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 	WPARAM wparam, LPARAM lparam)
@@ -22,6 +68,10 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	/*std::filesystem::create_directory("log");
+
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	std::chrono::time_point < std::chrono::system_clock, std::chrono::seconds >;*/
 	WNDCLASS wc{};
 	//windowプロシージャ
 	wc.lpfnWndProc = WindowProc;
@@ -71,7 +121,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}
 
 
-	OutputDebugStringA("Hello,DirectX!\n");
+	Log("Hello,DirectX!\n");
+	Log(ConvertString(
+		std::format(
+			L"clientSize:{} {}\n",
+			kClientWidth,
+			kClientHeight)));
 
 	return 0;
 }
