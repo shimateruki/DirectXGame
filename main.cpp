f2ffda343422d4f03f436eab0680fb77a053d499
shimateruki <k024g0032@g.neec.ac.jp>
2025-04-16 15:00:21 +0900

    windowの作成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,77 @@
-#include <windows.h>
+﻿#include <windows.h>
+#include <cstdint>
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
+	WPARAM wparam, LPARAM lparam)
+{
+
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		//windowsが放棄された
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	//標準メッセージを行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
+
+
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	WNDCLASS wc{};
+	//windowプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//windowクラス名
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	//windowクラスを登録する
+	RegisterClass(&wc);
+
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	//windowsの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName, // 利用するクラス名
+		L"CG2", // タイトルバーの文字
+		WS_OVERLAPPEDWINDOW, // よく見るウィンドウタイトル
+		CW_USEDEFAULT, // 表示X座標
+		CW_USEDEFAULT, // 表示Y座標
+		wrc.right - wrc.left, // ウィンドウ横幅
+		wrc.bottom - wrc.top, // ウィンドウ縦幅
+		nullptr, // 親ウィンドウハンドル
+		nullptr, // メニューハンドル
+		wc.hInstance, // インスタンスハンドル
+		nullptr); // オプション
+
+	ShowWindow(hwnd, SW_SHOW);
+	MSG msg{};
+	//windowの×ボタンが押されるまでループ
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else
+		{
+			//ゲームの処理
+
+		}
+
+	}
+
+
 	OutputDebugStringA("Hello,DirectX!\n");
+
 	return 0;
 }
