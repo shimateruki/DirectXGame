8941963afc5d726f6a462aeeffa53b53b330206e
shimateruki <k024g0032@g.neec.ac.jp>
2025-06-01 03:58:09 +0900

    リリース漏れの修正

--- main.cpp
+++ main.cpp
@@ -1275,6 +1275,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	textureResouces->Release();
 	intermediteResouces->Release();
 	depthStenscilResouces->Release();
+	dsvDescriptorHeap->Release();
 
 	
 
