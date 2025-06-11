3a3e6596d63a473ae89d7418e86aecdaa3cdb9e2
shimateruki <k024g0032@g.neec.ac.jp>
2025-06-11 15:19:41 +0900

    起動はできたけど実行結果が変わらない

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -12,7 +12,7 @@ ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 struct VertexShanderInput
 {
-    float32_t4 position :POSITION0;
+    float32_t4 position : POSITION0;
     float32_t2 texcoord : TEXCOORD0;
     float32_t3 normal : NORMAL0;
 };
@@ -22,6 +22,11 @@ VecrtexShaderOutput main(VertexShanderInput input)
     VecrtexShaderOutput output;
     output.position = mul(input.position, gTransformationMatrix.WVP);
     output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
+ 
+    
+
+    
     return output;
 }
 
