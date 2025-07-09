5e80ded91e1869eca4ac5d318379b500ba3e5230
shimateruki <k024g0032@g.neec.ac.jp>
2025-07-09 17:10:40 +0900

    まだ

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -33,10 +33,7 @@ SamplerState gSampler : register(s0);
 PixelShanderOutput main(VecrtexShaderOutput input)
 {
     PixelShanderOutput output;
-    //float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-
-    float4 transformedUV = mul(float32_t4(input.texcoord,0.0f, 1.0f), gMaterial.uvTransform);
-    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     output.color = gMaterial.color * textureColor;
     if (gMaterial.enableLighting != 0)
     {
@@ -52,5 +49,5 @@ PixelShanderOutput main(VecrtexShaderOutput input)
     
     }
       
-        return output;
+    return output;
 }
