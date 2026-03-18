07bf0cfc30c109cbe3989071ea55f62ee7de8da7
SoneTaisei <k024g0009@g.neec.ac.jp>
2026-03-18 16:13:05 +0900

    ボスのアニメーションを止めれるようにした。1キーで止める。

--- project/game/scene/GamePlayScene.cpp
+++ project/game/scene/GamePlayScene.cpp
@@ -192,13 +192,6 @@ void GamePlayScene::Update(float deltaTime) {
 	particleSystem_->Update(deltaTime);
 	objectManager_->Update(deltaTime); // オブジェクト一括更新
 
-	for (auto &obj : objectManager_->GetObjects ()) {
-		if (obj->GetName () == "Enemy_BossCore") {
-			// エンジンのスキップ処理を無視して、強制的に毎フレーム叩き起こす！
-			obj->Update (deltaTime);
-		}
-	}
-
 	// 例：座標(0, 5, 0) から、上方向(0, 10, 0) に向けて毎フレーム500個噴き出す
 	//GPUParticleManager::GetInstance()->Emit(
 	//	{ 0.0f, 5.0f, 0.0f },  // 発生座標
