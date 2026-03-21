78a0044a8c6403fd30801243f08158f4666a303a
shima_teruki <k024g0032@g.neec.ac.jp>
2026-03-21 17:19:14 +0900

    バグ修正

--- project/engine/utility/SystemDebug/editor/SpriteDebugEditor.cpp
+++ project/engine/utility/SystemDebug/editor/SpriteDebugEditor.cpp
@@ -298,6 +298,7 @@ void SpriteDebugEditor::DrawHierarchyWindow() {
             uint32_t handle = Sprite::LoadTexture(droppedFilename);
             newSprite->Initialize(spriteCommon, handle);
             newSprite->SetName(droppedFilename);
+            newSprite->SetTextureName(droppedFilename);
             sprites.push_back(std::move(newSprite));
             selectedSprite_ = sprites.back().get();
         }
@@ -391,6 +392,7 @@ void SpriteDebugEditor::DrawInspectorWindow() {
                 const char* droppedFilename = (const char*)payload->Data;
                 uint32_t newHandle = Sprite::LoadTexture(droppedFilename);
                 selectedSprite_->SetTextureHandle(newHandle);
+                selectedSprite_->SetTextureName(droppedFilename);
             }
             ImGui::EndDragDropTarget();
         }
@@ -476,6 +478,7 @@ void SpriteDebugEditor::SaveSpriteLayout(const std::string& filename) {
         spriteData["size"] = { size.x, size.y };
         spriteData["anchor"] = { anchor.x, anchor.y };
         spriteData["color"] = { color.x, color.y, color.z };
+        spriteData["texture"] = sprite->GetTextureName();
         spriteArray.push_back(spriteData);
     }
     root["sprites"] = spriteArray;
